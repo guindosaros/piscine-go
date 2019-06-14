166e172e1f6decb2fd0a55a3d16307cbbf2f22ba
guindosaros <47152090+guindosaros@users.noreply.github.com>
2019-06-14 00:41:59 +0000

    sortintegertable

---
+++ sortintegertable.go
@@ -0,0 +1,15 @@
+package piscine
+
+func SortIntegerTable(table []int) {
+	i := 1
+	for i < len(table) {
+		if table[i-1] > table[i] {
+			tmp := table[i]
+			table[i] = table[i-1]
+			table[i-1] = tmp
+			i = 1
+		} else {
+			i++
+		}
+	}
+}
