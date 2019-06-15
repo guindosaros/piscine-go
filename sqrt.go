e30852d877d5735582a0f317c3cf9ffaf931ed0e
guindosaros <47152090+guindosaros@users.noreply.github.com>
2019-06-15 02:11:46 +0100

    Update sqrt.go

--- sqrt.go
+++ sqrt.go
@@ -2,19 +2,14 @@ package piscine
 
 import "math"
 
-func Sqrt(nb int) int {
-	if nb < 0 || math.MaxInt32 < nb {
-		return 0
-	}
-	for i := 0; i < 101; i++ {
-		if nb == i*i {
-			nb = i
-			break
-		} else if i*i > nb {
-			nb = 0
-			break
-		}
-	}
-	return nb
-}
+func Sqrt(arg1 int) int{
 
+	argFlottant := float64(arg1)
+	var testeur float64 = float64(int(math.Sqrt(argFlottant)))
+
+	if math.Sqrt(argFlottant) - testeur != 0 {
+		return 0		
+	}else{
+		return int(math.Sqrt(argFlottant))
+	}
+}
